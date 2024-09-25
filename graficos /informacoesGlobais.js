1f95ca65e231a72a2fa3104c8c88323e0bf9f259
leegomes20 <105243028+leegomes20@users.noreply.github.com>
2024-09-25 10:52:44 -0300

    2509

--- graficos /informacoesGlobais.js
+++ graficos /informacoesGlobais.js
@@ -1,2 +1,9 @@
 const url = 'https://raw.githubusercontent.com/leticiagomesls/api/main/dados-globais.json'
 
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
+}
+
+vizualizarInformacoesGlobais()
