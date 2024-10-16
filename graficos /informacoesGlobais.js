f700d7f9d8d3f1c751f2e55513455e1fe39269d6
leegomes20 <105243028+leegomes20@users.noreply.github.com>
2024-10-16 10:38:18 -0300

    11

--- graficos /informacoesGlobais.js
+++ graficos /informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/legomes/api/main/dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
@@ -6,7 +6,7 @@ async function vizualizarInformacoesGlobais() {
     const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio)
-    const minutos = Math.round((dados.tempo_medio - horas) * 60)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
     const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
 
     const paragrafo = document.createElement('p')
